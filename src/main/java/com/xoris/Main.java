4ad3668f36a1c4ee247a78653c9d264149f794f7
Gabriel Evaristo <146457912+gabsevamac@users.noreply.github.com>
2026-09-25 13:46:57 -0300

    Feat: Adding the features into menu

--- src/main/java/com/xoris/Main.java
+++ src/main/java/com/xoris/Main.java
@@ -97,17 +97,19 @@ public class Main {
       case 4 -> tasks = getTaskByPeriodMenu();
       case 5 -> tasks = program.getAllTasks();
     }
-
+    clearConsole();
     if (tasks == null || tasks.isEmpty()) {
-      clearConsole();
       IO.println("NO TASK FOUND");
     } else {
-      clearConsole();
       IO.println("=== TASKS FOUNDED ===");
       for (int i = 0; i < tasks.size(); i++) {
-        IO.println(i+1 + "°:\n" + tasks.get(i));
+        IO.println("=========");
+        IO.println(i + 1 + "°:\n" + tasks.get(i));
+        IO.println("=========");
       }
     }
+
+    callModificationMenu(tasks);
   }
 
   public static List<Task> getTaskByPriorityMenu() {
@@ -174,6 +176,87 @@ public class Main {
     LocalDate end = LocalDate.parse(IO.readln("END DATE (YYYY-MM-DD): "));
 
     return program.getTasksByPeriod(start, end);
+  }
+
+  public static void callModificationMenu(List<Task> tasks) {
+    IO.println("=== WHAT WOULD YOU LIKE TO DO? ===");
+    IO.println("1 - MARK AS IN PROGRESS");
+    IO.println("2 - MARK AS COMPLETED");
+    IO.println("3 - MODIFY DETAILS");
+
+    int choose = Integer.parseInt(IO.readln("Choose an option: "));
+    int chosenTask = Integer.parseInt(IO.readln("Choose a task by number: "));
+    Task task = tasks.get(chosenTask);
+    switch (choose) {
+      case 1 -> {
+        program.markTaskAsInProgress(task);
+      }
+      case 2 -> {
+        program.markTaskAsCompleted(task);
+      }
+
+      case 3 -> {
+        callDetailsModificationMenu(task);
+      }
+    }
+  }
+
+  public static void callDetailsModificationMenu(Task task) {
+    clearConsole();
+    IO.println("=== CHOOSE THE MODIFICATION TYPE");
+    IO.println("1 - CHANGE SHORT DESCRIPTION");
+    IO.println("2 - CHANGE LONG DESCRIPTION");
+    IO.println("3 - CHANGE PRIORITY");
+    IO.println("4 - CHANGE CATEGORY");
+    IO.println("5 - CHANGE DUE DATE");
+
+    int choose = Integer.parseInt(IO.readln("Choose an option: "));
+    clearConsole();
+    switch (choose) {
+      case 1 -> {
+        String newDescription = IO.readln("New Short Description: ");
+        program.updateShortDescription(task, newDescription);
+      }
+      case 2 -> {
+        String newDescription = IO.readln("New Long Description: ");
+        program.updateLongDescription(task, newDescription);
+      }
+      case 3 -> {
+        IO.println("=== CHOOSE THE PRIORITY ===");
+        IO.println("1 - LOW");
+        IO.println("2 - MEDIUM");
+        IO.println("3 - HIGH");
+        IO.println("4 - CRITICAL");
+        var c = Integer.parseInt(IO.readln("Choose an option: "));
+
+        switch (c) {
+          case 1 -> program.updatePriority(task, Priority.LOW);
+          case 2 -> program.updatePriority(task, Priority.MEDIUM);
+          case 3 -> program.updatePriority(task, Priority.HIGH);
+          case 4 -> program.updatePriority(task, Priority.CRITICAL);
+        }
+      }
+
+      case 4 -> {
+        IO.println("=== CHOOSE THE CATEGORY ===");
+        IO.println("1 - STUDY");
+        IO.println("2 - WORK");
+        IO.println("3 - HOME");
+        IO.println("4 - OTHER");
+        var c = Integer.parseInt(IO.readln("Choose an option: "));
+
+        switch (c) {
+          case 1 -> program.updateCategory(task, TaskCategory.STUDY);
+          case 2 -> program.updateCategory(task, TaskCategory.WORK);
+          case 3 -> program.updateCategory(task, TaskCategory.HOME);
+          case 4 -> program.updateCategory(task, TaskCategory.OTHER);
+        }
+      }
+      case 5 -> {
+        var newDueDate = LocalDate.parse(IO.readln("Choose new due date (YYYY-MM-DD): "));
+        program.updateDueAt(task, newDueDate);
+      }
+    }
   }
 
   public static void clearConsole() {
--- src/test/java/ProgramTests.java
+++ src/test/java/ProgramTests.java
@@ -1,200 +1,199 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.xoris.enums.Priority;
 import com.xoris.enums.TaskCategory;
 import com.xoris.enums.TaskStatus;
 import com.xoris.models.Program;
 import com.xoris.models.Task;
+import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class ProgramTests {
-    private static Program program;
+  private static Program program;
 
-    @Nested
-    class SearchTests {
-        @BeforeEach()
-        void setup() {
-            program = new Program();
-            program.setTasks(
-                    List.of(
-                            new Task(
-                                    "Test 1 - World Building",
-                                    "New Test 1",
-                                    TaskCategory.STUDY,
-                                    Priority.CRITICAL,
-                                    LocalDate.parse("2026-09-30")),
-                            new Task(
-                                    "Test 2 - World Structures",
-                                    "New Test 2",
-                                    TaskCategory.HOME,
-                                    Priority.LOW,
-                                    LocalDate.parse("2026-09-29")),
-                            new Task(
-                                    "Test 3 - Other Testing",
-                                    "New Test 3",
-                                    TaskCategory.OTHER,
-                                    Priority.MEDIUM,
-                                    LocalDate.parse("2026-10-30")),
-                            new Task(
-                                    "Test 4 - Random Bullshit",
-                                    "New Test 4",
-                                    TaskCategory.WORK,
-                                    Priority.HIGH,
-                                    LocalDate.parse("2026-10-03"))));
-        }
-
-        @DisplayName("Get all tasks")
-        @Test
-        void shouldReturnAllTasks() {
-            List<Task> tasks = program.getAllTasks();
-            assertAll(
-                    () -> {
-                        assertNotNull(tasks);
-                        assertFalse(tasks.isEmpty());
-                    });
-        }
-
-        @DisplayName("Get all High Priority")
-        @Test
-        void shouldReturnHighestPriority() {
-            List<Task> tasks = program.getTasksByPriority(Priority.HIGH);
-            boolean check =
-                    !tasks.isEmpty()
-                            && tasks.stream().allMatch(task -> task.getPriority().equals(Priority.HIGH));
-
-            assertTrue(check);
-        }
-
-        @DisplayName("Get all Home category")
-        @Test
-        void shouldReturnHomeCategory() {
-            List<Task> tasks = program.getTasksByCategory(TaskCategory.HOME);
-            boolean check =
-                    !tasks.isEmpty()
-                            && tasks.stream().allMatch(task -> task.getCategory().equals(TaskCategory.HOME));
-            assertTrue(check);
-        }
-
-        @DisplayName("Get all with 'World' on shortDescription")
-        @Test
-        void shouldReturnAllWithWorldOnShortDescription() {
-            List<Task> tasks = program.getTasksByWord("World");
-            boolean check =
-                    !tasks.isEmpty()
-                            && tasks.stream()
-                            .allMatch(
-                                    task ->
-                                            task.getShortDescription().toLowerCase().contains("World".toLowerCase()));
-
-            assertTrue(check);
-        }
+  @Nested
+  class SearchTests {
+    @BeforeEach()
+    void setup() {
+      program = new Program();
+      program.setTasks(
+          List.of(
+              new Task(
+                  "Test 1 - World Building",
+                  "New Test 1",
+                  TaskCategory.STUDY,
+                  Priority.CRITICAL,
+                  LocalDate.parse("2026-09-30")),
+              new Task(
+                  "Test 2 - World Structures",
+                  "New Test 2",
+                  TaskCategory.HOME,
+                  Priority.LOW,
+                  LocalDate.parse("2026-09-29")),
+              new Task(
+                  "Test 3 - Other Testing",
+                  "New Test 3",
+                  TaskCategory.OTHER,
+                  Priority.MEDIUM,
+                  LocalDate.parse("2026-10-30")),
+              new Task(
+                  "Test 4 - Random Bullshit",
+                  "New Test 4",
+                  TaskCategory.WORK,
+                  Priority.HIGH,
+                  LocalDate.parse("2026-10-03"))));
     }
 
-    @Nested
-    class ModificationTests {
-        @BeforeEach()
-        void setup() {
-            program = new Program();
-            program.setTasks(
-                    List.of(
-                            new Task(
-                                    "Test 1 - World Building",
-                                    "New Test 1",
-                                    TaskCategory.STUDY,
-                                    Priority.CRITICAL,
-                                    LocalDate.parse("2026-09-30")),
-                            new Task(
-                                    "Test 2 - World Structures",
-                                    "New Test 2",
-                                    TaskCategory.HOME,
-                                    Priority.LOW,
-                                    LocalDate.parse("2026-09-29")),
-                            new Task(
-                                    "Test 3 - Other Testing",
-                                    "New Test 3",
-                                    TaskCategory.OTHER,
-                                    Priority.MEDIUM,
-                                    LocalDate.parse("2026-10-30")),
-                            new Task(
-                                    "Test 4 - Random Bullshit",
-                                    "New Test 4",
-                                    TaskCategory.WORK,
-                                    Priority.HIGH,
-                                    LocalDate.parse("2026-10-03"))));
-        }
-
-        @DisplayName("Mark Task As In Progress")
-        @Test
-        void shouldMarkTaskAsInProgress() {
-            Task task = program.getAllTasks().getFirst();
-            program.markTaskAsInProgress(task);
-            boolean check = task.getStatus().equals(TaskStatus.IN_PROGRESS);
-
-            assertTrue(check);
-        }
-
-        @DisplayName("Mark Task As Completed")
-        @Test
-        void shouldMarkTaskAsCompleted() {
-            Task task = program.getAllTasks().getFirst();
-            program.markTaskAsCompleted(task);
-            boolean check = task.getStatus().equals(TaskStatus.COMPLETED);
-
-            assertTrue(check);
-        }
-
-        @DisplayName("Update Short Description")
-        @Test
-        void shouldUpdateShortDescription() {
-            Task task = program.getAllTasks().getFirst();
-            String newDescription = "New Test 1";
-            program.updateShortDescription(task, newDescription);
-
-            assertEquals(newDescription, task.getShortDescription());
-        }
-
-        @DisplayName("Update Long Description")
-        @Test
-        void shouldUpdateLongDescription() {
-            Task task = program.getAllTasks().getFirst();
-            String newDescription = "New Test 1 long Description";
-            program.updateLongDescription(task, newDescription);
-
-            assertEquals(newDescription, task.getLongDescription());
-        }
-
-        @DisplayName("Update Priority")
-        @Test
-        void shouldUpdatePriority() {
-            Task task = program.getAllTasks().getFirst();
-            Priority newPriority = Priority.MEDIUM;
-
-            program.updatePriority(task, newPriority);
-            assertEquals(newPriority, task.getPriority());
-        }
-
-        @DisplayName("Update Category")
-        @Test
-        void shouldUpdateCategory() {
-            Task task = program.getAllTasks().getFirst();
-            TaskCategory newCategory = TaskCategory.OTHER;
-            program.updateCategory(task, newCategory);
-            assertEquals(newCategory, task.getCategory());
-        }
-
-        @DisplayName("Update Due Date")
-        @Test
-        void shouldUpdateDueDate() {
-            Task task = program.getAllTasks().getFirst();
-            LocalDate newDueDate = LocalDate.parse("2027-01-01");
-            program.updateDueAt(task, newDueDate);
-            assertEquals(newDueDate, task.getDueDate());
-        }
+    @DisplayName("Get all tasks")
+    @Test
+    void shouldReturnAllTasks() {
+      List<Task> tasks = program.getAllTasks();
+      assertAll(
+          () -> {
+            assertNotNull(tasks);
+            assertFalse(tasks.isEmpty());
+          });
     }
+
+    @DisplayName("Get all High Priority")
+    @Test
+    void shouldReturnHighestPriority() {
+      List<Task> tasks = program.getTasksByPriority(Priority.HIGH);
+      boolean check =
+          !tasks.isEmpty()
+              && tasks.stream().allMatch(task -> task.getPriority().equals(Priority.HIGH));
+
+      assertTrue(check);
+    }
+
+    @DisplayName("Get all Home category")
+    @Test
+    void shouldReturnHomeCategory() {
+      List<Task> tasks = program.getTasksByCategory(TaskCategory.HOME);
+      boolean check =
+          !tasks.isEmpty()
+              && tasks.stream().allMatch(task -> task.getCategory().equals(TaskCategory.HOME));
+      assertTrue(check);
+    }
+
+    @DisplayName("Get all with 'World' on shortDescription")
+    @Test
+    void shouldReturnAllWithWorldOnShortDescription() {
+      List<Task> tasks = program.getTasksByWord("World");
+      boolean check =
+          !tasks.isEmpty()
+              && tasks.stream()
+                  .allMatch(
+                      task ->
+                          task.getShortDescription().toLowerCase().contains("World".toLowerCase()));
+
+      assertTrue(check);
+    }
+  }
+
+  @Nested
+  class ModificationTests {
+    @BeforeEach()
+    void setup() {
+      program = new Program();
+      program.setTasks(
+          List.of(
+              new Task(
+                  "Test 1 - World Building",
+                  "New Test 1",
+                  TaskCategory.STUDY,
+                  Priority.CRITICAL,
+                  LocalDate.parse("2026-09-30")),
+              new Task(
+                  "Test 2 - World Structures",
+                  "New Test 2",
+                  TaskCategory.HOME,
+                  Priority.LOW,
+                  LocalDate.parse("2026-09-29")),
+              new Task(
+                  "Test 3 - Other Testing",
+                  "New Test 3",
+                  TaskCategory.OTHER,
+                  Priority.MEDIUM,
+                  LocalDate.parse("2026-10-30")),
+              new Task(
+                  "Test 4 - Random Bullshit",
+                  "New Test 4",
+                  TaskCategory.WORK,
+                  Priority.HIGH,
+                  LocalDate.parse("2026-10-03"))));
+    }
+
+    @DisplayName("Mark Task As In Progress")
+    @Test
+    void shouldMarkTaskAsInProgress() {
+      Task task = program.getAllTasks().getFirst();
+      program.markTaskAsInProgress(task);
+      boolean check = task.getStatus().equals(TaskStatus.IN_PROGRESS);
+
+      assertTrue(check);
+    }
+
+    @DisplayName("Mark Task As Completed")
+    @Test
+    void shouldMarkTaskAsCompleted() {
+      Task task = program.getAllTasks().getFirst();
+      program.markTaskAsCompleted(task);
+      boolean check = task.getStatus().equals(TaskStatus.COMPLETED);
+
+      assertTrue(check);
+    }
+
+    @DisplayName("Update Short Description")
+    @Test
+    void shouldUpdateShortDescription() {
+      Task task = program.getAllTasks().getFirst();
+      String newDescription = "New Test 1";
+      program.updateShortDescription(task, newDescription);
+
+      assertEquals(newDescription, task.getShortDescription());
+    }
+
+    @DisplayName("Update Long Description")
+    @Test
+    void shouldUpdateLongDescription() {
+      Task task = program.getAllTasks().getFirst();
+      String newDescription = "New Test 1 long Description";
+      program.updateLongDescription(task, newDescription);
+
+      assertEquals(newDescription, task.getLongDescription());
+    }
+
+    @DisplayName("Update Priority")
+    @Test
+    void shouldUpdatePriority() {
+      Task task = program.getAllTasks().getFirst();
+      Priority newPriority = Priority.MEDIUM;
+
+      program.updatePriority(task, newPriority);
+      assertEquals(newPriority, task.getPriority());
+    }
+
+    @DisplayName("Update Category")
+    @Test
+    void shouldUpdateCategory() {
+      Task task = program.getAllTasks().getFirst();
+      TaskCategory newCategory = TaskCategory.OTHER;
+      program.updateCategory(task, newCategory);
+      assertEquals(newCategory, task.getCategory());
+    }
+
+    @DisplayName("Update Due Date")
+    @Test
+    void shouldUpdateDueDate() {
+      Task task = program.getAllTasks().getFirst();
+      LocalDate newDueDate = LocalDate.parse("2027-01-01");
+      program.updateDueAt(task, newDueDate);
+      assertEquals(newDueDate, task.getDueDate());
+    }
+  }
 }
